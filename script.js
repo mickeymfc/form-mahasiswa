91a6481274970fb3830ac2e2d4b9074b7e4f15c5
mickeymfc <mickeydrive01@gmail.com>
2026-01-15 15:15:58 +0800

    Modify fetch request in form submission
    Updated fetch request to use 'no-cors' mode and removed headers.

--- script.js
+++ script.js
@@ -16,18 +16,10 @@ form.addEventListener("submit", function (e) {
 
   fetch("https://script.google.com/macros/s/AKfycbx9vGzRRvG2Ameo52GZm18UrKdjoS_JwS-RbH-VKcigm2pNY3qJuYcsaW-IEMNKIw0HfQ/exec", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    mode: "no-cors",
     body: JSON.stringify(data)
-  })
-  .then(res => res.text())
-  .then(() => {
-    alert("Data terkirim");
-    form.reset();
-  })
-  .catch(err => {
-    console.error(err);
-    alert("Gagal");
   });
+
+  alert("Data terkirim");
+  form.reset();
 });
