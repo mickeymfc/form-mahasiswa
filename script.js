0bb49a81b70d4db866960fd7530c3b90e3b20b83
mickeymfc <mickeydrive01@gmail.com>
2026-01-15 15:06:27 +0800

    Modify form submission handler in script.js
    Updated fetch URL and alert messages for form submission.

--- script.js
+++ script.js
@@ -8,21 +8,20 @@ form.addEventListener("submit", function (e) {
     semester: semester.value
   };
 
-  fetch("https://script.google.com/macros/s/AKfycbzZl7FrVFYaqIKE0UqIDXkuvE6msFSVM4q4SD2mDyZm/dev", {
+  fetch("https://script.google.com/macros/s/AKfycbx9vGzRRvG2Ameo52GZm18UrKdjoS_JwS-RbH-VKcigm2pNY3qJuYcsaW-IEMNKIw0HfQ/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
   })
-  .then(res => res.text())   // Apps Script aman pakai text
+  .then(res => res.text())
   .then(() => {
-    alert("Data terkirim ke Google Sheet");
+    alert("Data terkirim");
     form.reset();
   })
   .catch(err => {
     console.error(err);
-    alert("Gagal mengirim data");
+    alert("Gagal");
   });
 });
-
